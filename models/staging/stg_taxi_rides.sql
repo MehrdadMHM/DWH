bb19e79345599d5444cb86594b8eccd96eed7ae0
Mehrdad Hosseini Mehr <HosseiniMehr.Mehrdad@gmail.com>
2026-08-07 22:18:45 +0000

    neu

--- models/staging/stg_taxi_rides.sql
+++ models/staging/stg_taxi_rides.sql
@@ -1,11 +1,15 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
 with source as (
-    -- خواندن مستقیم از جدول برنز که در دیتابریکس داریم
     select * from {{ source('default', 'TaxiData_Bronze') }}
 ),
 
 cleaned as (
     select
-        -- ساخت کلید هش (Hash Key) برای ساختار Data Vault
         md5(cast(VendorID as string)) as trip_hkey,
         VendorID as vendor_id,
         tpep_pickup_datetime as pickup_datetime,
