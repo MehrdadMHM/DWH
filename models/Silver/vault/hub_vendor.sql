d4393171abceaca89cc01e68bf26021eb2d69d16
Mehrdad Hosseini Mehr <HosseiniMehr.Mehrdad@gmail.com>
2026-09-09 22:23:11 +0000

    beu File

--- models/Silver/vault/hub_vendor.sql
+++ models/Silver/vault/hub_vendor.sql
@@ -1,14 +1,18 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='trip_hkey'
-    )
-}}
+{{ config(
+    materialized='incremental',
+    unique_key='trip_hkey'
+) }}
 
-select distinct
+select 
     trip_hkey,
-    pickup_zip as business_key,
+    min(pickup_zip) as business_key,
     current_timestamp() as load_date,
     'nyctaxi_trips' as record_source
 from {{ ref('stg_taxi_rides') }}
 where pickup_zip is not null
+group by trip_hkey, current_timestamp(), record_source
+
+{% if is_incremental() %}
+    -- این شرط باید بعد از یک WHERE یا AND درست قرار بگیرد
+    having current_timestamp() > (select max(load_date) from {{ this }})
+{% endif %}
